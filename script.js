495c8c9a0874a0eeda7cac954b0b2e983c5665ab
Joshua Peoples <jpcoder12@gmail.com>
2022-11-17 01:11:03 -0700

    add mode and stage, clearOutput function
    also added superscript icon

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ let val2 = ''
 let outputDisplay = ''
 let answer = ''
 // mode dictates what to do when a function button is clicked. (2 modes: primary & secondary)
-let mode = 'primary'
+let stage = 'primary'
+let mode = '' // whether it's + - x or /
 const setOutput = (num) => {
     // If this is the first set of numbers, append to outputDisplay. 
-    // you cannot go beyond 15 chars so it will just not take your selection
-    if (mode == 'primary'){
+    // you cannot go beyond 15 chars so it will just not take your selected button's input
+    if (stage == 'primary'){
         if (outputDisplay.length <= 14){
-            outputDisplay == '0' ? outputDisplay = '' :
-            outputDisplay+= num
+            if (outputDisplay == '0'){
+                outputDisplay = ''
+            }
+            outputDisplay += num
             document.querySelector("#output-text").innerHTML = outputDisplay
         } 
     }
+    
 }
 
 const clearOutput = () => {
     // if the outputDisplay is already 0, then both variables to perform ops are cleared. 
     // if the outputDisplay is not clear, this function just clears the current output to 0.
+    if (outputDisplay == '0'){
+        val1 = ''
+        val2 = ''
+        document.querySelector("#output-text").innerHTML = outputDisplay
+    } 
+    else if (outputDisplay != '0'){
+        outputDisplay = '0'
+        document.querySelector("#output-text").innerHTML = outputDisplay
+    }
     
 }
